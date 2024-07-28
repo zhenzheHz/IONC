729d92ede42db9ebc11c9068e09eaf1cb51e9349
zhenzheHz <109124xdf@gmail.com>
2024-07-29 00:01:00 +0800

    add Day1_pA.cpp

---
+++ Day1_pA.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int int64_t
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0),cout.tie(0)
+#define endl '\n'
+using namespace std;
+int32_t main(){
+    fastio;
+    string s;
+    cin>>s;
+    if(s == "LJH"){
+        cout << "LJH is baganono.";
+    }else cout << "Hello, ionc.";
+}
